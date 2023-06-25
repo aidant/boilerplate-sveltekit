552573bc45ba724bea28a93f0802c5f072a967e7
aidant <15520814+aidant@users.noreply.github.com>
2023-06-25 14:08:12 +1000

    update packages and restore home styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="mx-2 my-4 text-4xl">Welcome to SvelteKit</h1>
+<p class="m-2"
+  >Visit <a
+    class="text-blue-500 underline"
+    href="https://kit.svelte.dev">kit.svelte.dev</a
+  > to read the documentation</p
+>
